945839b21e7eaa34abb56cb7d84464a33fd5ae37
Roman Hosek <roman.hosek@24i.com>
2021-08-01 15:37:57 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 
 hub checkout master
 pwd
-ls
-ls app
+
 ls app/build/outputs/
-ls app/build/outputs/release/
+ls app/build/outputs/apk/
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/${FLAVOUR}/app-${FLAVOUR}-release.apk -m "Build# ${RELEASE_TITLE}" $(date +%Y%m%d%H%M%S) 
