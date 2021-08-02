2db30e3957d646204c5fdae6c722039809400694
Roman Hosek <roman.hosek@24i.com>
2021-08-02 08:37:51 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 hub checkout master
 pwd
 
-ls app/build/outputs/
 ls app/build/outputs/apk/
+ls app/build/outputs/apk/romotop/
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/${FLAVOUR}/app-${FLAVOUR}-release.apk -m "Build# ${RELEASE_TITLE}" $(date +%Y%m%d%H%M%S) 
